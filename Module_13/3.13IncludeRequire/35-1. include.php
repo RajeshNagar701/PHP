4c1a5c105fc9f86412570fcfc824b4654266227e
GauravGoswami12967 <gaurav12967@gmail.com>
2025-03-20 14:49:25 +0530

    first commit

--- Module_13/3.13IncludeRequire/35-1. include.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'file_1.php';
-echo "<h1>This is home page.</h1>";
-include_once 'file_2.php';
-include_once 'file_2.php';
-include_once 'file_2.php';
-
-?>
--- Module_13/3.13IncludeRequire/35-2. require.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'file_1.php';
-echo "<h1>This is home page.</h1>";
-require_once 'file_2.php';
-require_once 'file_2.php';
-require_once 'file_2.php';
-
-?>
