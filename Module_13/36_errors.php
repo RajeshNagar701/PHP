8bfe92dd5ecbb47f47e882a1192e23a4ef8593b6
GauravGoswami12967 <gaurav12967@gmail.com>
2025-03-25 13:11:58 +0530

    first commit

---
+++ Module_13/36_errors.php
@@ -0,0 +1,39 @@
+<?php
+
+// Warning Error.
+/*
+	include 'undefined.php';
+	echo "This is errors file";
+*/
+
+// Notice Error.
+/*
+	$test = 'Defined Variable';
+	echo $test1;
+	echo "<hr>";
+	echo $test;
+*/
+
+// Parse or Syntax Error
+/*	echo "BMW";
+	echo "Bentley";
+	echo "Audi";
+	echo "Skoda";
+	echo "Ford";
+*/
+
+// Fatal Error
+
+	function add(){
+		$add = 5+5;
+		echo "Addition is: $add";
+	}
+
+	add1();
+
+
+
+
+
+
+?>
